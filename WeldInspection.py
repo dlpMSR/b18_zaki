9d5bae05cff409afa500f8f01641a70b642ae013
DLPMSR-PEACH\Zechs <pntsec_msr@outlook.com>
2019-01-19 17:16:03 +0900

    Add processing for displaying graphs

--- WeldInspection.py
+++ WeldInspection.py
@@ -1,5 +1,7 @@
 import numpy as np
 import cv2
+import matplotlib as mpl
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import time
 
@@ -12,8 +14,9 @@ def weldInspection():
     cap = cv2.VideoCapture('./V_20190114_155120_vHDR_On_Trim.mp4')
     fourcc = cv2.VideoWriter_fourcc(*'MJPG')
     out = cv2.VideoWriter('output.avi', fourcc, 30.0, (3840, 2160))
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
     while(cap.isOpened()):
-        t_start = time.time()
         ret, frame = cap.read()
         if ret == True:
             frame = ImageProcessing(frame)
@@ -21,20 +24,21 @@ def weldInspection():
             hilightedFrame = frame.generateHighlightedSurfaceImage(frameWithStats)
             out.write(hilightedFrame)
             if frameWithStats.isDetected() == True:
-                # To do : 上手く検出されたときの処理を記述
-                print(frameWithStats.maxHeightOfD)
-                print(frameWithStats.posInDepthDirection)
+                z = frameWithStats.surfaceOfD[1]
+                y = frameWithStats.surfaceOfD[0]
+                x = np.full(z.size, frameWithStats.posInDepthDirection)
+                ax.plot(x, y, z, color='green', linewidth=1)
             cv2.imshow('frame', hilightedFrame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
         else:
             break
-        t_end = time.time()
-        elapsed_time = t_end - t_start
-        print('1loop:{}'.format(elapsed_time))
     cap.release()
     out.release()
     cv2.destroyAllWindows()
+    
+    ax.set_zlim(800, 1600)
+    plt.show()
 
 
 def main():

eb99fcbb93ed816ee3af7d59e45bbe276a5c3284
DLPMSR-PEACH\Zechs <pntsec_msr@outlook.com>
2019-01-20 21:15:44 +0900

    Add processing to write the result to the image

--- components/imageProcessing.py
+++ components/imageProcessing.py
@@ -41,3 +41,23 @@ class ImageProcessing(object):
             for i in range(len(surface[0])):
                 hilightedImage[surface[1][i], surface[0][i]] = (0, 255, 0)
         return hilightedImage
+
+    def generateOutputImage(self, frameWithStatus):
+        output_image = self.image
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        if frameWithStatus.isDetected() == True:
+            B = frameWithStatus.B
+            C = frameWithStatus.C
+            D = frameWithStatus.D
+            cv2.line(output_image, B.getBottomLine()[0], B.getBottomLine()[1], (0, 255, 0), 10)
+            cv2.putText(output_image, 'B:{}'.format(B.length),
+                        (B.centroid[0], B.centroid[1]-30), font, 3, (0, 255, 0), 3, cv2.LINE_AA)
+            cv2.line(output_image, C.getBottomLine()[0], C.getBottomLine()[1], (0, 255, 0), 10)
+            cv2.putText(output_image, 'C:{}'.format(C.length),
+                        (C.centroid[0], C.centroid[1]-30), font, 3, (0, 255, 0), 3, cv2.LINE_AA)
+            cv2.line(output_image, B.rearEndCoordinates, (B.rearEndCoordinates[0], C.frontEndCoordinates[1]), (255, 0, 255), 5)
+            cv2.line(output_image, D.getBottomLine()[0], D.getBottomLine()[1], (255, 255, 0), 10)
+            cv2.rectangle(output_image, (D.maxOfHeight[0]-20, D.maxOfHeight[1]), (D.maxOfHeight[0]+20, D.getBottomLine()[0][1]), (255, 255, 0), thickness=-1)
+            cv2.putText(output_image, 'D_height:{}'.format(D.getBottomLine()[0][1]-D.maxOfHeight[1]),
+                        (D.centroid[0]+200, D.centroid[1]-30), font, 3, (255, 255, 0), 3, cv2.LINE_AA)
+        return output_image
